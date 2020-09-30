f32a9b9036eb350df25359c617f7d749406b85b8
kaluram09980 <72165707+kaluram09980@users.noreply.github.com>
2020-10-01 01:31:00 +0530

    Update 11.1 word-count-better-sorted.py.py

--- 11.1 word-count-better-sorted.py.py
+++ 11.1 word-count-better-sorted.py.py
@@ -15,8 +15,5 @@ wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
 results = wordCountsSorted.collect()
 
 for result in results:
-    count = str(result[0])
-    word = result[1].encode('ascii', 'ignore')
-    if (word):
-        print(word.decode() + ":\t\t" + count)
-        print("Spark demo")
+    count = str(
+        new changes
